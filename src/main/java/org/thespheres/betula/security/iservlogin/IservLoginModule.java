296dd636732e5b424c17d383688f50a1aac800d4
Boris Heithecker <boris.heithecker@gmx.net>
2020-06-08 15:50:12 +0200

    cacertsaccess, custom props

--- src/main/java/org/thespheres/betula/security/iservlogin/IservLoginModule.java
+++ src/main/java/org/thespheres/betula/security/iservlogin/IservLoginModule.java
@@ -6,6 +6,7 @@
 package org.thespheres.betula.security.iservlogin;
 
 import com.sun.appserv.security.AppservPasswordLoginModule;
+import com.sun.enterprise.security.auth.realm.BadRealmException;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -24,33 +25,40 @@ import org.apache.commons.net.imap.IMAPSClient;
  * @author boris.heithecker
  */
 public class IservLoginModule extends AppservPasswordLoginModule {
- 
+    
     @Override
     protected void authenticateUser() throws LoginException {
         String[] grpList = authorize();
         if (grpList == null || grpList.length == 0) {
             throw new LoginException();
         }
-
+        
         boolean success = false;
         AuthenticatingIMAPClient cl;
         try {
-            cl = new AuthenticatingIMAPClient(IMAPSClient.DEFAULT_PROTOCOL, true);
+            cl = new AuthenticatingIMAPClient(IMAPSClient.DEFAULT_PROTOCOL, true, getIservRealm().getSSLContext());
+            cl.setEndpointCheckingEnabled(getIservRealm().isEndpointChecking());
             cl.connect(getIservRealm().getIservImapHost(), getIservRealm().getIservImapPort());
         } catch (IOException ex) {
             Logger.getLogger(IservLoginModule.class.getCanonicalName()).log(Level.WARNING, "Could not establish connection to iserv.", ex);
             throw new LoginException();
+        } catch (BadRealmException ex) {
+            throw new IllegalStateException(ex);
         }
-
+        
         try {
             success = cl.authenticate(AuthenticatingIMAPClient.AUTH_METHOD.PLAIN, this._username, new String(this._passwd));
             cl.logout();
         } catch (IOException ex) {
             Logger.getLogger(IservLoginModule.class.getCanonicalName()).log(Level.WARNING, "Failed to log in on iserv.", ex);
-            throw new LoginException();
+            final LoginException lex = new LoginException();
+            lex.initCause(ex);
+            throw lex;
         } catch (NoSuchAlgorithmException | InvalidKeyException | InvalidKeySpecException ex) {
             Logger.getLogger(IservLoginModule.class.getName()).log(Level.SEVERE, null, ex);
-            throw new LoginException();
+            final LoginException lex = new LoginException();
+            lex.initCause(ex);
+            throw lex;
         } finally {
             try {
                 cl.disconnect();
@@ -61,23 +69,27 @@ public class IservLoginModule extends AppservPasswordLoginModule {
             commitUserAuthentication(grpList);
         }
     }
-
+    
     private String[] authorize() throws LoginException {
         try {
-            Properties props = new Properties();
+            final Properties props = new Properties();
             getIservRealm().addProperties(props);
-            IservLogin lb = (IservLogin) new InitialContext(props).lookup(Constants.LOGIN_BEAN_NAME);
-            return lb.getGroups(this._username, getIservRealm().getIservImapHost());
-        } catch (NamingException ex) {
+            final IservLogin lb = (IservLogin) new InitialContext(props).lookup(Constants.LOGIN_BEAN_NAME);
+            final String sfx = getIservRealm().getSigneeSuffix();
+            final String suffix = sfx != null ? sfx : getIservRealm().getIservImapHost();
+            return lb.getGroups(this._username, suffix);
+        } catch (final NamingException ex) {
             Logger.getLogger(IservLoginModule.class.getName()).log(Level.SEVERE, null, ex);
-            throw new LoginException();
+            final LoginException lex = new LoginException();
+            lex.initCause(ex);
+            throw lex;
         }
     }
-
+    
     private IservRealm getIservRealm() throws LoginException {
         try {
             return (IservRealm) getCurrentRealm();
-        } catch (ClassCastException e) {
+        } catch (final ClassCastException e) {
             throw new LoginException();
         }
     }
--- src/main/java/org/thespheres/betula/security/iservlogin/IservRealm.java
+++ src/main/java/org/thespheres/betula/security/iservlogin/IservRealm.java
@@ -11,12 +11,22 @@ import com.sun.enterprise.security.auth.realm.InvalidOperationException;
 import com.sun.enterprise.security.auth.realm.NoSuchRealmException;
 import com.sun.enterprise.security.auth.realm.NoSuchUserException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.KeyStore;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 import org.apache.commons.net.imap.AuthenticatingIMAPClient;
 import org.apache.commons.net.imap.IMAPSClient;
 
@@ -26,17 +36,23 @@ import org.apache.commons.net.imap.IMAPSClient;
  */
 public class IservRealm extends AppservRealm {
 
+    static final String DEFAULT_PASSWORD = "changeit";
     static final String[] GROUPS = {"signees"};
     private String host;
     private int port;
+    private boolean endpointChecking;
+    private String signeeSuffix;
     private String endpoints;
+    private SSLContext ssl;
 
     @Override
     protected void init(Properties props) throws BadRealmException, NoSuchRealmException {
         super.init(props);
-        String h = props.getProperty("iserv.imap.host");
-        String pt = props.getProperty("iserv.imap.port");
-        String ep = props.getProperty("com.sun.appserv.iiop.endpoints");
+        final String h = props.getProperty("iserv.imap.host");
+        final String pt = props.getProperty("iserv.imap.port");
+        final String hv = props.getProperty("iserv.imap.checkEndpoint");
+        final String suffix = props.getProperty("iserv.imap.signee-suffix");
+        final String ep = props.getProperty("com.sun.appserv.iiop.endpoints");
         if (h == null || pt == null || h.isEmpty() || pt.isEmpty()) {
             throw new NoSuchRealmException("No iserv host and/or no iserv port provided.");
         }
@@ -51,15 +67,21 @@ public class IservRealm extends AppservRealm {
         }
         AuthenticatingIMAPClient cl = null;
         try {
-            cl = new AuthenticatingIMAPClient(IMAPSClient.DEFAULT_PROTOCOL, true);
+            cl = new AuthenticatingIMAPClient(IMAPSClient.DEFAULT_PROTOCOL, true, getSSLContext());
+            //Hostname verfications must be disabled because
+            //server may be running on IServ as host machine
+            final boolean checkEndpoint = Boolean.parseBoolean(hv);
+            cl.setEndpointCheckingEnabled(checkEndpoint);
             cl.connect(h, p);
             host = h;
             port = p;
+            signeeSuffix = (suffix == null || suffix.trim().isEmpty()) ? null : suffix.trim();
+            endpointChecking = checkEndpoint;
             if (ep != null && !ep.trim().isEmpty()) {
                 this.endpoints = ep.trim();
             }
         } catch (IOException ex) {
-            BadRealmException baex = new BadRealmException("Could not connect to: " + h + ":" + p, ex);
+            final BadRealmException baex = new BadRealmException("Could not connect to: " + h + ":" + p, ex);
             Logger.getLogger(IservRealm.class.getName()).log(Level.SEVERE, baex.getMessage(), baex);
             throw baex;
         } finally {
@@ -84,16 +106,24 @@ public class IservRealm extends AppservRealm {
     }
 
     @Override
-    public Enumeration getGroupNames(String string) throws InvalidOperationException, NoSuchUserException {
+    public Enumeration getGroupNames(final String string) throws InvalidOperationException, NoSuchUserException {
         return Collections.enumeration(Arrays.asList(GROUPS));
     }
 
-    String getIservImapHost() {
+    public String getIservImapHost() {
         return host;
     }
 
-    int getIservImapPort() {
+    public int getIservImapPort() {
         return port;
+    }
+
+    public boolean isEndpointChecking() {
+        return endpointChecking;
+    }
+
+    public String getSigneeSuffix() {
+        return signeeSuffix;
     }
 
     void addProperties(Properties props) {
@@ -102,4 +132,29 @@ public class IservRealm extends AppservRealm {
             props.put("com.sun.appserv.iiop.endpoints", endpoints);
         }
     }
+
+    SSLContext getSSLContext() throws BadRealmException {
+        if (ssl == null) {
+            try {
+                final SSLContext ctx = SSLContext.getInstance("TLSv1.3");
+                final KeyManagerFactory kstorefac = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                final Path kspath = Paths.get(System.getProperty("javax.net.ssl.keyStore"));
+                final KeyStore kstore = KeyStore.getInstance(System.getProperty("javax.net.ssl.keyStoreType", KeyStore.getDefaultType()));
+                kstore.load(Files.newInputStream(kspath, StandardOpenOption.READ), DEFAULT_PASSWORD.toCharArray());
+                kstorefac.init(kstore, DEFAULT_PASSWORD.toCharArray());
+                final KeyManager[] kms = kstorefac.getKeyManagers();
+                final TrustManagerFactory tstorefac = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                final Path tspath = Paths.get(System.getProperty("javax.net.ssl.trustStore"));
+                final KeyStore tstore = KeyStore.getInstance(System.getProperty("javax.net.ssl.trustStoreType", KeyStore.getDefaultType()));
+                tstore.load(Files.newInputStream(tspath, StandardOpenOption.READ), DEFAULT_PASSWORD.toCharArray());
+                tstorefac.init(tstore);
+                ctx.init(kms, tstorefac.getTrustManagers(), new SecureRandom());
+                ssl = ctx;
+            } catch (final Exception ex) {
+                throw new BadRealmException(ex);
+            }
+        }
+        return ssl;
+    }
+
 }
